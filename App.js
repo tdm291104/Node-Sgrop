74465e8303a4e106e69b802f5ff01e05005fabd2
tdm <tu.manh291104@gmail.com>
2024-05-16 21:20:17 +0700

    update code

--- App.js
+++ App.js
@@ -4,72 +4,60 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-// app.use(bodyParser.json())
+
 
 const data = JSON.parse(fs.readFileSync('data.json'))
 
 app.get('/', (req, res) => {
+  res.send("Hello World!!!!!!!!")
+})
+
+app.get('/api/users', (req, res) => {
   res.json(data);
 })
-
-app.post('/', (req, res) => {
-  const a = req.body
-  data.push(a)
-  fs.writeFileSync('data.json', JSON.stringify(data))
-  res.status(201)
-  res.send("Thêm thành công")
-})
-
-app.get('/:id', (req, res) => {
-  // console.log(req.params);
-  const id = parseInt(req.params.id);
+app.get('/api/users/:id', (req, res) => {
+  const id = parseInt(req.params.id)
   const object = data.find((obj) => {
     if(id == obj.id)
       return obj
     else return null
-  });
-  // console.log(object)
-  if (object) {
-    res.json(object);
-  } else {
-    res.status(404)
-  }
-});
+    });
 
-app.put('/:id', (req, res)=>{
-  const id = parseInt(req.params.id)
-  const update = req.body
-  const index = data.findIndex((obj)=>{
-    if(id == obj.id)
-      return obj
-    else return null
-  });
-  if (index !== -1) {
-    // data.id = update.id
-    // data.title = update.title
-    // data.author = update.author
-    // data.year = update.year
-    data[index] = { ...data[index], ...update };
-    fs.writeFileSync('data.json', JSON.stringify(data));
-    res.send('Sửa thành công');
-  }
+    if (object) {
+      res.json(object);
+      
+    }
 })
 
-app.delete('/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const index = data.findIndex((obj)=>{
-    if(id == obj.id)
-      return obj
-    else return null
-  });
-  if (index !== -1) {
-    
-    data.splice(index, 1);
+app.post('/api/users', (req, res) => {
+  req.body.id = parseInt(data[(data.length)-1].id)+1
+  data.push(req.body)
+  fs.writeFileSync('data.json', JSON.stringify(data))
+  res.send("Success")
+})
 
-    fs.writeFileSync('data.json', JSON.stringify(data));
-    res.send('Xóa thành công');
-  }
-});
+app.put('/api/users/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const update = req.body
+  data.forEach((element,x) => {
+    if(element.id == id){
+      data[x].name = update.name
+    }
+  })
+  fs.writeFileSync('data.json', JSON.stringify(data))
+  res.send("Susses")
+})
+
+app.delete('/api/users/:id', (req, res) =>{
+  const id = parseInt(req.params.id);
+  data.forEach((e,x)=>{
+    if(e.id === id){
+      data.splice(x,1)
+    }
+  })
+  fs.writeFileSync('data.json', JSON.stringify(data))
+  res.send("Susses")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
