209aa5f1eed76343e361d684cdebbf04200f9cc7
TuanAnh1003 <anhaanh2003@gmail.com>
2024-05-18 15:12:14 +0700

    review/v1

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ const port = 3000
 
 app.use(express.json())
 
-
 const data = JSON.parse(fs.readFileSync('data.json'))
 
 app.get('/', (req, res) => {
@@ -16,7 +15,9 @@ app.get('/api/users', (req, res) => {
   res.json(data);
 })
 app.get('/api/users/:id', (req, res) => {
+  // what if id is not a number?
   const id = parseInt(req.params.id)
+  // what if this id doesn't exist in data?
   data.forEach((e) => {
     if(id == e.id)
       res.json(e)
@@ -24,32 +25,40 @@ app.get('/api/users/:id', (req, res) => {
 })
 
 app.post('/api/users', (req, res) => {
+  // what if user provide data not following your the schema?
   req.body.id = parseInt(data[(data.length)-1].id)+1
   data.push(req.body)
   fs.writeFileSync('data.json', JSON.stringify(data))
+  // What if the write operation fail?
   res.send("Success")
 })
 
 app.put('/api/users/:id', (req, res) => {
+  // what if id is not a number?
   const id = parseInt(req.params.id)
   const update = req.body
+  // what if this id doesn't exist in data?
   data.forEach((e,x) => {
     if(e.id == id){
       data[x].name = update.name
     }
   })
   fs.writeFileSync('data.json', JSON.stringify(data))
+  // What if the write operation fail?
   res.send("Susses")
 })
 
 app.delete('/api/users/:id', (req, res) =>{
+  // what if id is not a number?
   const id = parseInt(req.params.id);
+  // what if this id doesn't exist in data?
   data.forEach((e,x)=>{
     if(e.id === id){
       data.splice(x,1)
     }
   })
   fs.writeFileSync('data.json', JSON.stringify(data))
+  // What if the write operation fail?
   res.send("Susses")
 })
 
