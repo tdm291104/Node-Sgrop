72ff70e16b6b8423e5f401df7a378ba2b8a38141
tdm <tu.manh291104@gmail.com>
2024-05-20 09:29:34 +0700

    Update

--- App.js
+++ App.js
@@ -16,42 +16,70 @@ app.get('/api/users', (req, res) => {
   res.json(data);
 })
 app.get('/api/users/:id', (req, res) => {
-  const id = parseInt(req.params.id)
+  const id = req.params.id
+  let usercheck = false;
   data.forEach((e) => {
     if(id == e.id)
+      usercheck = true
       res.json(e)
-    });
+  });
+
+  if(!usercheck){
+    res.send("Fail")
+  }
+
 })
 
 app.post('/api/users', (req, res) => {
-  const news = req.body
-  news.id = parseInt(data[(data.length)-1].id)+1
-  data.push(news)
-  fs.writeFileSync('data.json', JSON.stringify(data))
-  res.send("Success")
+  try{
+    const news = req.body
+    news.id = parseInt(data[(data.length)-1].id)+1
+    data.push(news)
+    fs.writeFileSync('data.json', JSON.stringify(data))
+    res.send("Success")
+  }
+  catch(e){
+    res.send("Fail")
+  }
 })
 
 app.put('/api/users/:id', (req, res) => {
-  const id = parseInt(req.params.id)
+  const id = req.params.id
   const update = req.body
+  let usercheck = false;
   data.forEach((e,x) => {
     if(e.id == id){
+      let usercheck = true;
       data[x].name = update.name
     }
   })
-  fs.writeFileSync('data.json', JSON.stringify(data))
-  res.send("Susses")
+  if(usercheck){
+    fs.writeFileSync('data.json', JSON.stringify(data))
+    res.send("Susses")
+  }
+  else{
+    res.send("Fail")
+  }
+  
 })
 
 app.delete('/api/users/:id', (req, res) =>{
-  const id = parseInt(req.params.id);
+  const id = req.params.id;
+  let usercheck = false;
   data.forEach((e,x)=>{
     if(e.id === id){
+      usercheck = true
       data.splice(x,1)
     }
   })
-  fs.writeFileSync('data.json', JSON.stringify(data))
-  res.send("Susses")
+  if(usercheck){
+    fs.writeFileSync('data.json', JSON.stringify(data))
+    res.send("Susses")
+  }
+  else{
+    res.send("Fail")
+  }
+  
 })
 
 app.listen(port, () => {
